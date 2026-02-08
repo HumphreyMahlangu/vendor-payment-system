4c88a0fef73323940d0bb6a892124cddf3cdde94
HumphreyMahlangu <godasamukelo757@gmail.com>
2026-02-08 17:39:45 +0200

    chore: initial project setup with spring dependencies

---
+++ src/main/java/za/ac/mycput/vendorpaymentsystem/VendorPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package za.ac.mycput.vendorpaymentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendorPaymentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendorPaymentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/za/ac/mycput/vendorpaymentsystem/VendorPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.mycput.vendorpaymentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendorPaymentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
