101c86683f4dee8683ace70e7ccf6ea739055371
HumphreyMahlangu <godasamukelo757@gmail.com>
2026-02-10 12:21:36 +0200

    feat: add VendorRepository with custom finders

---
+++ src/main/java/za/ac/mycput/vendorpaymentsystem/repository/VendorRepository.java
@@ -0,0 +1,13 @@
+package za.ac.mycput.vendorpaymentsystem.repository;
+
+import za.ac.mycput.vendorpaymentsystem.model.Vendor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface VendorRepository extends JpaRepository<Vendor, Long> {
+    Optional<Vendor> findByEmail(String email);
+    boolean existsByTaxId(String taxId);
+}
